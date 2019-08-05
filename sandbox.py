89574faff5ce789899b5b4bf618695d323bf6cda
William Stevens <contact@wastevensv.com>
2019-08-05 00:25:26 -0400

    Fixed static variable issue.

--- sandbox.py
+++ sandbox.py
@@ -9,9 +9,8 @@ import functools as F
 from tempfile import TemporaryFile
 
 class BaseNode:
-    framerate = 44100
-    sampwidth = 2
-    
+    framerate = 192000
+
     def __call__(self, signal=None, **kwargs):
         if signal:
             if not callable(signal):
@@ -21,17 +20,17 @@ class BaseNode:
                     raise TypeError("{} is not a signal type.".format(type(signal)))
             self.sink(signal, **kwargs)
         return self.source()
-    
+
     def sink(self, signal):
         raise Exception("{} has no sink.".format(type(self)))
-    
+
     def source(self):
         return None
 
 class ConstantNode(BaseNode):
     def __init__(self, value=1):
         self.value = value
-    
+
     def source(self):
         while True:
             yield self.value
@@ -39,56 +38,72 @@ class ConstantNode(BaseNode):
 class SineNode(BaseNode):
     def __init__(self, freq=440):
         self.freq = freq
-    
+
     def source(self):
         for i in I.count(0):
             yield math.sin((i/self.framerate)*math.tau*self.freq)
 
 class MixNode(BaseNode):
-    _sinks = []
-    _weights = []
+
+    def __init__(self):
+        self._sinks = []
+        self._weights = []
 
     def sink(self, signal, weight=1):
         self._sinks.append(signal)
         self._weights.append(weight)
-    
+
     def source(self):
         mag = sum(self._weights)
-        if mag > 1:
-            weights = [ n/mag for n in self._weights]
-            
+        if mag > 1.0:
+            weights = [ n/mag for n in self._weights ]
+        else:
+            weights = self._weights
+
         while True:
-            yield sum(map(Op.mul,map(next,self._sinks),self._weights))
+            yield sum(map(Op.mul,map(next,self._sinks),weights))
 
 class DumpNode(BaseNode):
-    _sink = None
-    
+    def __init__(self):
+        self._sink = None
+
     def sink(self, signal):
         self._sink = signal
 
     def dump(self, output, duration=1):
-        max_amplitude = float(int((2 ** (self.sampwidth * 8)) / 2) - 1)
         numsamples = int(duration * self.framerate)
 
         for sample in I.islice(self._sink, numsamples):
-            if sample > 1: sample = 1
-            if sample < -1: sample = -1
-            frame = struct.pack('h', int(max_amplitude * sample))
+            if sample > 1: sample = 1.0
+            if sample < -1: sample = -1.0
+            frame = struct.pack('f', sample)
             output.write(frame)
+
+def harmony(f0=440,nharmonics=3):
+    mixer = MixNode()
+
+    for h in range(1,nharmonics+1):
+        tone = SineNode(f0*h)
+        mixer(tone())
+
+    return mixer
 
 
 def _main(outfile):
-    null = ConstantNode(0)
-    osc = SineNode(4410)
+    osc1 = harmony(360,2)
+    osc2 = harmony(640,2)
+
     mixer = MixNode()
+
     out = DumpNode()
 
-    mixer(osc())
+    mixer(osc1())
+    mixer(osc2())
 
     out(mixer())
 
     out.dump(outfile)
-    
+
 
 if __name__ == "__main__":
     from sys import argv
